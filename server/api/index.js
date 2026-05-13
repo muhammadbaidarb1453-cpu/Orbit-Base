a801c86ea09522fbb90a7bf3e18f053599af8bd0
muhammadbaidarb1453-cpu <muhammadbaidar.b1453@gmail.com>
2026-05-14 04:59:58 +0500

    Catch module load errors for diagnostics

--- server/api/index.js
+++ server/api/index.js
@@ -1,1 +1,9 @@
-module.exports = require('../src/index');
+let app;
+try {
+  app = require('../src/index');
+} catch (e) {
+  app = (req, res) => {
+    res.status(500).json({ error: e.message, type: e.constructor.name });
+  };
+}
+module.exports = app;
